2c7c3013c93b1465c6bd874b098eb54e8a6daf40
Adam Kim <adamkim@umich.edu>
2020-11-11 14:38:10 -0500

    tab title

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import { Typography, Toolbar } from '@material-ui/core';
+import { Typography, Toolbar, Button } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Header() {
+    const handleClick = (event) => {
+        event.stopPropagation();
+        const githubURL = 'https://github.com/akdiam/michigantime';
+        window.open(githubURL);
+    }
     return (
         <div className="header">
             <AppBar position="static" style={{height:"4vh"}} color="secondary">
-                <Toolbar><Typography variant="h6" style={{paddingBottom:'25px'}}>michigantime</Typography></Toolbar>
+                <Toolbar>
+                    <Typography variant="h6" style={{paddingBottom:'25px'}}>michigantime</Typography>
+                    <Button onClick={handleClick} color="inherit" style={{paddingBottom:'30px'}}><GitHubIcon/></Button>
+                </Toolbar>
             </AppBar>
         </div>
     )
